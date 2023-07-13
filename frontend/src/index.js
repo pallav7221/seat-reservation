f5e8a40067cda57dc746c994fd8d1f19a8b00948
pallav7221 <pallavaj72218@gmail.com>
2023-07-13 22:02:59 +0530

    deployed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
 
-axios.defaults.baseURL =  "http://localhost:8080"
+axios.defaults.baseURL =  "https://seat-reservation-pallav7221.vercel.app/"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
